a20b9ef8eb83e45118dbd9db8db3224aee737e46
Wojciech Turek <wojciech.turek@smartive.app>
2022-03-02 15:02:01 +0100

    test class added to project

---
+++ src/test/java/com/backbase/bestPictureAwards/BestPictureAwardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backbase.bestPictureAwards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestPictureAwardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
